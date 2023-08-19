909f02f646918eeb44a10e662bfcab1406929174
ilhammmaulana <ilhammmaulana688@gmail.com>
2023-08-19 08:55:09 +0700

    initial commite

---
+++ scroll.js
@@ -0,0 +1,16 @@
+console.log('About Developer : https://github.com/ilhammmaulana')
+
+const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+
+        if (item.isIntersecting) {
+            observer.unobserve(item.target);
+            item.target.classList.add('show-scroll-animation')
+        } else {
+            item.target.classList.remove('show-scroll-animation')
+        }
+    })
+})
+
+const hideElements = document.querySelectorAll('.hide-scroll-animation')
+hideElements.forEach((e) => observer.observe(e))
